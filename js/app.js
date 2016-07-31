e1b5e839380b3b4f8622159fcbb639a478494bca
Michael Q <MichaelQuiban@outlook.com>
2016-07-30 22:24:19 -0700

    Added HTML, CSS, light jQuery.
    Added Quiz information on a new page.
    Added format for how quizzes will look on each page.
    *Header 1
    *Header 2
    *Form Radio buttons
    *Next Question
    
    Removed first page from DOM.

---
+++ js/app.js
@@ -0,0 +1,27 @@
+$("#beginContainer").remove();
+
+var comedy_questions = [{
+  question_string: "What color is the sky?",
+  choices: {
+    correct: "Blue",
+    wrong: ["Pink", "Orange", "Green"]
+  }
+}, {
+  question_string: "Which season is the hottest?",
+  choices: {
+    correct: "Summer",
+    wrong: ["Winter", "Autumn", "Spring"]
+  }
+}, {
+  question_string: "How many wheels are there on a tricycle?",
+  choices: {
+    correct: "Three",
+    wrong: ["One", "Two", "Four"]
+  }
+}, {
+  question_string: 'Who is the main character of Harry Potter?',
+  choices: {
+    correct: "Harry Potter",
+    wrong: ["Hermione Granger", "Ron Weasley", "Voldemort"]
+  }
+}];
