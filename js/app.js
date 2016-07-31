26d8a55d10a1ecea53a773bf5eadb829a5a61e2e
Michael Q <MichaelQuiban@outlook.com>
2016-07-31 10:02:04 -0700

    Added 4 Questions and finished structure.
    Added format for each quiz.
    Added Styles for each quiz.
    Remaining is the javascript.

--- js/app.js
+++ js/app.js
@@ -1,27 +1,32 @@
 $("#beginContainer").remove();
+$("#secondContainer").remove();
+$("#thirdContainer").remove();
+$("#fourthContainer").remove();
+$("#fifthContainer").remove();
+$("sixthContainer").remove();
 
 var comedy_questions = [{
-  question_string: "What color is the sky?",
-  choices: {
-    correct: "Blue",
-    wrong: ["Pink", "Orange", "Green"]
+  	question_string: "Placeholder",
+  	choices: {
+    correct: "Placeholder",
+    wrong: ["Placeholder", "Placeholder", "Placeholder"]
   }
 }, {
-  question_string: "Which season is the hottest?",
-  choices: {
-    correct: "Summer",
-    wrong: ["Winter", "Autumn", "Spring"]
+ 	question_string: "Placeholder",
+	choices: {
+    correct: "Placeholder",
+    wrong: ["Placeholder", "Placeholder", "Placeholder"]
   }
 }, {
-  question_string: "How many wheels are there on a tricycle?",
-  choices: {
-    correct: "Three",
-    wrong: ["One", "Two", "Four"]
+    question_string: "Placeholder",
+  	choices: {
+    correct: "Placeholder",
+    wrong: ["Placeholder", "Placeholder", "Placeholder"]
   }
 }, {
-  question_string: 'Who is the main character of Harry Potter?',
-  choices: {
-    correct: "Harry Potter",
-    wrong: ["Hermione Granger", "Ron Weasley", "Voldemort"]
+     question_string: "Placeholder",
+ 	 choices: {
+     correct: "Placeholder",
+     wrong: ["Placeholder", "Placeholder", "Placeholder"]
   }
 }];
